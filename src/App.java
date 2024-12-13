cd66383f131ad06fc202e783d5723ca9d6798c4d
vinayrakesh <vinayrakesh123@gmail.com>
2024-12-13 17:28:45 -0500

    Added Subject details

--- src/App.java
+++ src/App.java
@@ -5,7 +5,9 @@ public class App {
         System.out.println("Hello User!");
         Scanner sc=new Scanner(System.in);
         String fname,lname,gender,place;
-        int age;
+        int age,total,maxMarks;
+        String sub1,sub2,sub3,sub4;
+
         System.out.println("Please Enter First name,last name,age,gender,place");
         fname=sc.nextLine();
         lname=sc.nextLine();
@@ -13,6 +15,13 @@ public class App {
         sc.nextLine();
         gender=sc.nextLine();
         place=sc.nextLine();
+
+
+        System.out.println("Please Enter All the 4 Subject names and marks obtained");
+        sub1=sc.next();
+        sub2=sc.nextLine();
+        sub3=sc.nextLine();
+        sub4=sc.nextLine();
 
         System.out.println("User Info Given :-\nfirst name :-> "+fname+" \nlast name :-> "+lname+" \nage :-> "+age+"\ngender :-> "+gender+"\nplace :-> "+place);
 
